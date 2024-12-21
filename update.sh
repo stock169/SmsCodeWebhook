e902f90280767a6ba94e689aac44891206a6d66e
stock169 <58556976+stock169@users.noreply.github.com>
2024-12-21 15:24:38 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,6 @@
-    
+apt update
+apt-get -y install dialog apt-utils
+apt -y upgrade
+rm requirements.txt
+wget https://raw.githubusercontent.com/stock169/SmsCodeWebhook/refs/heads/master/requirements.txt
+pip install -r requirements.txt
