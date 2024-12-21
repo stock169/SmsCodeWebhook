1be173ba4c42595dd395cbbe3278daab66d22e95
stock169 <58556976+stock169@users.noreply.github.com>
2024-12-21 22:00:51 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 apt update
-apt-get -y install dialog apt-utils
+apt-get -y install dialog apt-utils redis-server
 apt -y upgrade
 rm requirements.txt
 wget https://raw.githubusercontent.com/stock169/SmsCodeWebhook/refs/heads/master/requirements.txt
